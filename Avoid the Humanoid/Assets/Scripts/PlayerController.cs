978129a75b3cac4ddb7111b2dae7765d95b79b45
mmalcolm02 <mmalcolm02@qub.ac.uk>
2023-10-20 19:09:44 +0100

    Initial Commit
    Initial development

---
+++ Avoid the Humanoid/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public float rangeX = 40;
+    public float rangeZ = 40;
+    public Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        PlayerContraints();
+    }
+    //Player Movement based on translate function
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRB.AddForce(Vector3.right * horizontalInput * speed);
+        playerRB.AddForce(Vector3.forward * verticalInput * speed);
+    }
+
+    //Player Constrained by boundaries
+
+    void PlayerContraints()
+    {
+        if (transform.position.x > rangeX)
+        {
+            transform.position = new Vector3(rangeX, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -rangeX)
+        {
+            transform.position = new Vector3(-rangeX, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > rangeZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, rangeZ);
+        }
+        if (transform.position.z < -rangeZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -rangeZ);
+        }
+    }
+}

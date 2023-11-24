90865ac5269bc6519e27f3292abe603462e5e338
mmalcolm02 <mmalcolm02@qub.ac.uk>
2023-11-24 12:30:18 +0000

    Animation, camera and canvas changes

---
+++ Avoid the Humanoid/Assets/Scripts/MainCameraMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraMovement : MonoBehaviour
+{
+
+    public PlayerController player;
+    public float yOffset = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       transform.position = new Vector3 (0,yOffset, player.transform.position.z);
+
+
+    }
+}
